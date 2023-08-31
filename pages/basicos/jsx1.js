de8664a3ec0833257899c912f190d1cb9d68a301
FelipeEnne <felipeenne@gmail.com>
2023-08-31 18:12:39 -0300

    add jsx2

---
+++ pages/basicos/jsx1.js
@@ -0,0 +1,4 @@
+//JSX
+export default function JSX1() {
+  return <h1>JSX #01</h1>;
+}
---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,12 @@
+//JSX
+export default function JSX2() {
+  const array = [<li>Paulo</li>, <li>Ana</li>, <li>Rebeca</li>];
+
+  const conteudo = (
+    <div>
+      <h1>JSX #02</h1>
+    </div>
+  );
+
+  return conteudo;
+}
