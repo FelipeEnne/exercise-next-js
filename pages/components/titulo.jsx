96f2691005cb0d711322f21dfedb5cf43f868b54
FelipeEnne <felipeenne@gmail.com>
2023-09-05 23:43:41 -0300

    add component

---
+++ pages/components/titulo.jsx
@@ -0,0 +1,9 @@
+export default function Titulo() {  
+  const conteudo = (
+    <div>
+      <h1>titulo</h1>
+    </div>
+  );
+
+  return conteudo;
+}
---
+++ pages/components/usandoTitulo.jsx
@@ -0,0 +1,11 @@
+import Titulo from "../components/Titulo";
+
+export default function usandoTitulo() {  
+  const conteudo = (
+    <>
+      <Titulo/>
+    </>
+  );
+
+  return conteudo;
+}
