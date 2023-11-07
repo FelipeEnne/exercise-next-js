4198e04655a131dcccd5264f0a545133cbddc6f9
FelipeEnne <felipeenne@gmail.com>
2023-11-06 23:29:35 -0300

    integração com css

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,9 @@
+export default function integracao(params) {
+  return (
+    <div className="intergracao1">
+      <div className="vermelha">Texto #01</div>
+      <div className="azul">Texto #03</div>
+      <div className="branco">Texto #02</div>
+    </div>
+  )
+}
