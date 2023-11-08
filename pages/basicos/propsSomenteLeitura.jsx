fa996d54f3fead440c1fdb66efdc660f69d6d954
FelipeEnne <felipeenne@gmail.com>
2023-11-07 21:30:51 -0300

    add somente um

---
+++ pages/basicos/propsSomenteLeitura.jsx
@@ -0,0 +1,9 @@
+import SomaUm from "../components/SomaUm";
+
+export default function propsSomenteLeitura(params) {
+  return (
+    <div className="">
+      <SomaUm numero={99}/>
+    </div>
+  )
+}
---
+++ pages/components/SomaUm.jsx
@@ -0,0 +1,8 @@
+export default function SomaUm(props) {
+  // props.numero++
+  return (
+    <div className="">
+      <h1>{props.numero+1}</h1>
+    </div>
+  )
+}
